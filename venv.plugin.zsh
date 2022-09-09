e4a32649dae46571841975a1feb0dcd532686519
Filip Godlewski <filip.godlewski@outlook.com>
2022-09-09 21:05:41 +0200

    feat: 🎸 make auto automatic and optional

--- venv.plugin.zsh
+++ venv.plugin.zsh
@@ -1,5 +1,9 @@
 [[ -z "$VENVPLUG" ]] && export VENVPLUG="${${(%):-%x}:a:h}"
 [[ -z "$VENVSBASEPATH" ]] && export VENVSBASEPATH="$XDG_DATA_HOME/venvs"
+if [[ -z "$VENVPLUGNOAUTO" -o "$VENVPLUGNOAUTO" == true ]]; then 
+  export VENVPLUGNOAUTO=true
+  function chpwd() { venv auto }
+fi
 
 for config_file ("$VENVPLUG"/lib/**/*.zsh); do
   source "$config_file"
