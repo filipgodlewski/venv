f29d1cd9341b8fed33705c2376ed3248984c0203
Filip Godlewski <filip.godlewski@outlook.com>
2022-09-12 22:54:09 +0200

    fix: 🐛 minor changes to new

--- lib/new.zsh
+++ lib/new.zsh
@@ -7,7 +7,7 @@ EOF
     return 0
 }
 function _venv::new {
-  zparseopts -D -E -a flags h -help -activate
+  zparseopts -D -E -a flags h -help -activate -no-link
   zparseopts -D -E -A opts -project-path:
 
   ((${flags[(Ie)-h]} > 0 || ${flags[(Ie)--help]} > 0)) && {$0::help; return 0}
@@ -21,6 +21,7 @@ function _venv::new {
   [[ $is_linked == true ]] && {echo "'$project_path' is already assigned to venv named '$name'"; return 2}
   if [[ -d $venv_path ]]; then
     echo "Venv '$name' already exists and is assigned to another project(s)."
+    ((${flags[(Ie)--no-link]} > 0)) && return 1
     echo "Do you want to reuse it for the current project? [Y/n] \c"
     read answer
 
@@ -34,12 +35,12 @@ function _venv::new {
         ;;
     esac
     echo "$project_path" >> $venv_path/.venv_paths
+    echo "Linked to '$project_path'"
   else
     python3 -m venv $venv_path --system-site-packages --upgrade-deps
-    echo "$project_path" > $venv_path/.venv_paths
     echo "\nNew venv created under: '$venv_path'"
+    ((${flags[(Ie)--no-link]} > 0)) && {echo "$project_path" > $venv_path/.venv_paths; echo "Linked to '$project_path'"}
   fi
 
-  echo "Linked to '$project_path'"
   ((${flags[(Ie)--activate]} > 0)) && source $venv_path/bin/activate
 }
