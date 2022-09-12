359bdcb15c927284396d45be3f516cc3c6f38c0e
Filip Godlewski <filip.godlewski@outlook.com>
2022-09-12 23:43:10 +0200

    fix: 🐛 wrong name alternative in helper

--- lib/helpers.zsh
+++ lib/helpers.zsh
@@ -3,7 +3,7 @@
 function _venv::_get_venv_info {
   zparseopts -D -E -A opts -name: -project-path:
   local project_path=${opts[--project-path]:-${$(git rev-parse --show-toplevel 2> /dev/null):-$PWD}}
-  local name=${opts[--name]:-${opts[--project-path]##*/:-$(echo ${project_path##*/} | sed 's/ /_/g')}}
+  local name=${opts[--name]:-$(echo ${project_path##*/} | sed 's/ /_/g')}
   local venv_path=$VENVSBASEPATH/$name
   [[ $(grep -x $project_path $venv_path/.venv_paths 2> /dev/null) ]] && local is_linked=true || local is_linked=false
 
